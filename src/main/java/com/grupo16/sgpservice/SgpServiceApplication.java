a292d6ac1d52671600a8346b2d9e2b6025b1b110
tatiane <tati.guisso@gmail.com>
2023-10-13 10:59:38 -0300

    initial commit

---
+++ src/main/java/com/grupo16/sgpservice/SgpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.grupo16.sgpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo16/sgpservice/SgpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo16.sgpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
