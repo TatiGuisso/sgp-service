e062d2c3c9bd32f900faa8c944abe66cefb03db8
Tatiane <tati.guisso@gmail.com>
2023-10-13 16:48:15 -0300

    Implementando estacionamento checkin

--- src/main/java/com/grupo16/sgpservice/gateway/adapter/RegistroEstacionamentoMongoGateway.java
+++ src/main/java/com/grupo16/sgpservice/gateway/adapter/RegistroEstacionamentoMongoGateway.java
@@ -1,5 +1,7 @@
 package com.grupo16.sgpservice.gateway.adapter;
 
+import org.springframework.stereotype.Component;
+
 import com.grupo16.sgpservice.domain.RegistroEstacionamentoBase;
 import com.grupo16.sgpservice.exception.ErroAcessoBancoDadosException;
 import com.grupo16.sgpservice.gateway.RegistroEstacionamentoRepositoryGateway;
@@ -7,6 +9,7 @@ import com.grupo16.sgpservice.gateway.RegistroEstacionamentoRepositoryGateway;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 public class RegistroEstacionamentoMongoGateway implements RegistroEstacionamentoRepositoryGateway {
 
 	@Override
