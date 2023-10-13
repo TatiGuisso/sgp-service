efc5d56be9a17745beda9df1cca442026c2646a5
tatiane <tati.guisso@gmail.com>
2023-10-13 11:56:49 -0300

    feat: adicionado novo domain Condutor

---
+++ src/main/java/com/grupo16/sgpservice/domain/Condutor.java
@@ -0,0 +1,16 @@
+package com.grupo16.sgpservice.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class Condutor {
+	
+	private String id;
+	private String nome;
+	private String cpf;
+	private String email;
+	private String telefone;
+
+}
