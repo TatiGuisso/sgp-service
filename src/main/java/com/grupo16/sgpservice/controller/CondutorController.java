f2d160c961558a0557acb22545a9bc0acb299de2
tatiane <tati.guisso@gmail.com>
2023-10-13 14:11:01 -0300

    implementando Controller: criar e alterar condutor

--- src/main/java/com/grupo16/sgpservice/controller/CondutorController.java
+++ src/main/java/com/grupo16/sgpservice/controller/CondutorController.java
@@ -1,26 +1,50 @@
 package com.grupo16.sgpservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.grupo16.sgpservice.controller.json.CondutorJson;
+import com.grupo16.sgpservice.domain.Condutor;
+import com.grupo16.sgpservice.usecase.CriarAlterarCondutorUseCase;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RequestMapping()
+@RequestMapping("condutores")
 @RestController
 public class CondutorController {
 	
+	@Autowired
+	private CriarAlterarCondutorUseCase criarAlterarCondutorUseCase;
+	
 	public String criar(
-			@RequestBody(required=true)CondutorJson condutorJson) {
+			@RequestBody(required = true)CondutorJson condutorJson) {
 		log.trace("Start condutorJson={}", condutorJson);
 		
+		Condutor condutor = condutorJson.mapearParaCondutorDomain();
 		
+		String id = criarAlterarCondutorUseCase.criar(condutor);
 		
-//		log.trace("End id={}", id);
-		return null;
+		log.trace("End id={}", id);
+		return id;
+	}
+	
+	@PutMapping("{id}")
+	public void alterar(
+			@PathVariable(required = true, name = "id") String id,
+			@RequestBody(required = true) CondutorJson condutorJson) {
+		log.trace("Start id={}, condutorJson={}", id, condutorJson);
+		
+		Condutor condutor = condutorJson.mapearParaCondutorDomain();
+		
+		criarAlterarCondutorUseCase.alterar(condutor);
+		
+		log.trace("End");
+		
 	}
 
 }
---
+++ src/main/java/com/grupo16/sgpservice/usecase/CriarAlterarCondutorUseCase.java
@@ -0,0 +1,17 @@
+package com.grupo16.sgpservice.usecase;
+
+import com.grupo16.sgpservice.domain.Condutor;
+
+public class CriarAlterarCondutorUseCase {
+
+	public String criar(Condutor condutor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void alterar(Condutor condutor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
