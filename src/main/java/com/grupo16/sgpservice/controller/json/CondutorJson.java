d579e3806e1bc32c39db3d9a7d628c7dcab9805c
tatiane <tati.guisso@gmail.com>
2023-10-13 11:58:40 -0300

    feat: adicionado CondutorJson

---
+++ src/main/java/com/grupo16/sgpservice/controller/json/CondutorJson.java
@@ -0,0 +1,15 @@
+package com.grupo16.sgpservice.controller.json;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class CondutorJson {
+
+	private String id;
+	private String nome;
+	private String cpf;
+	private String email;
+	private String telefone;
+}
